d8868b435ee1adf95b8267caf57a94ae24e48abb
CodeMonkey626 <CodeMonkey626@users.noreply.github.com>
2024-07-15 19:13:26 -0400

    Initial commit

--- MapQuests/Plugin.cs
+++ MapQuests/Plugin.cs
@@ -1,7 +1,6 @@
 ﻿using Dalamud.Game.Command;
 using Dalamud.IoC;
 using Dalamud.Plugin;
-using System.IO;
 using Dalamud.Interface.Windowing;
 using Dalamud.Plugin.Services;
 using SamplePlugin.Windows;
@@ -13,38 +12,29 @@ public sealed class Plugin : IDalamudPlugin
     [PluginService] internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
     [PluginService] internal static ITextureProvider TextureProvider { get; private set; } = null!;
     [PluginService] internal static ICommandManager CommandManager { get; private set; } = null!;
+    [PluginService] public static IDataManager DataManager { get; private set; } = null!;
 
-    private const string CommandName = "/pmycommand";
+    private const string CommandName = "/pmq";
 
     public Configuration Configuration { get; init; }
 
-    public readonly WindowSystem WindowSystem = new("SamplePlugin");
-    private ConfigWindow ConfigWindow { get; init; }
+    public readonly WindowSystem WindowSystem = new("MapQuests");
     private MainWindow MainWindow { get; init; }
 
     public Plugin()
     {
         Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
 
-        // you might normally want to embed resources and load them from the manifest stream
-        var goatImagePath = Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png");
+        MainWindow = new MainWindow(this);
 
-        ConfigWindow = new ConfigWindow(this);
-        MainWindow = new MainWindow(this, goatImagePath);
-
-        WindowSystem.AddWindow(ConfigWindow);
         WindowSystem.AddWindow(MainWindow);
 
         CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
         {
-            HelpMessage = "A useful message to display in /xlhelp"
+            HelpMessage = "Toggle the display of the quest list."
         });
 
         PluginInterface.UiBuilder.Draw += DrawUI;
-
-        // This adds a button to the plugin installer entry of this plugin which allows
-        // to toggle the display status of the configuration ui
-        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;
 
         // Adds another button that is doing the same but for the main ui of the plugin
         PluginInterface.UiBuilder.OpenMainUi += ToggleMainUI;
@@ -54,7 +44,6 @@ public sealed class Plugin : IDalamudPlugin
     {
         WindowSystem.RemoveAllWindows();
 
-        ConfigWindow.Dispose();
         MainWindow.Dispose();
 
         CommandManager.RemoveHandler(CommandName);
@@ -68,6 +57,5 @@ public sealed class Plugin : IDalamudPlugin
 
     private void DrawUI() => WindowSystem.Draw();
 
-    public void ToggleConfigUI() => ConfigWindow.Toggle();
     public void ToggleMainUI() => MainWindow.Toggle();
 }
---
+++ MapQuests/Windows/MainWindow.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+using Dalamud.Interface.Utility;
+using Dalamud.Interface.Utility.Raii;
+using Dalamud.Interface.Windowing;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
+using Map = FFXIVClientStructs.FFXIV.Client.Game.UI.Map;
+using Quest = Lumina.Excel.GeneratedSheets.Quest;
+using ImGuiNET;
+using Dalamud.Interface;
+
+namespace SamplePlugin.Windows;
+
+public unsafe class MainWindow : Window, IDisposable
+{
+    private Plugin Plugin;
+    private const float ElementHeight = 48.0f;
+
+    // We give this window a hidden ID using ##
+    // So that the user will see "My Amazing Window" as window title,
+    // but for ImGui the ID is "My Amazing Window##With a hidden ID"
+    public MainWindow(Plugin plugin)
+        : base("Unaccepted Quests", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    {
+        SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(300.0f, 500.0f),
+            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+        };
+
+        Plugin = plugin;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+        using var child = ImRaii.Child($"quest_list_scrollable_child", new Vector2(0.0f, 0.0f), false, ImGuiWindowFlags.None);
+
+        if (Map.Instance()->UnacceptedQuestMarkers.Count > 0) {
+            foreach (var quest in Map.Instance()->UnacceptedQuestMarkers) {
+                var questData = Plugin.DataManager.GetExcelSheet<Quest>()!.GetRow(quest.ObjectiveId + 65536u);
+                
+                foreach (var marker in quest.MarkerData) {
+                    var cursorStart = ImGui.GetCursorScreenPos();
+                    if (ImGui.Selectable($"##{quest.ObjectiveId}_Selectable_{marker.LevelId}", false, ImGuiSelectableFlags.None, new Vector2(ImGui.GetContentRegionAvail().X, ElementHeight * ImGuiHelpers.GlobalScale))) {
+                        //AgentMap.Instance()->OpenMapByMapId(marker.MapId, 0, true);
+                    }
+
+                    ImGui.SetCursorScreenPos(cursorStart);
+                    ImGui.Image(Plugin.TextureProvider.GetFromGameIcon(marker.IconId).GetWrapOrEmpty().ImGuiHandle, ImGuiHelpers.ScaledVector2(ElementHeight, ElementHeight));
+                    
+                    ImGui.SameLine();
+                    var text = $"Lv. {questData?.ClassJobLevel0} {quest.Label}";
+                    
+                    ImGui.SetCursorPosY(ImGui.GetCursorPosY() + ElementHeight * ImGuiHelpers.GlobalScale / 2.0f - ImGui.CalcTextSize(text).Y / 2.0f);
+                    ImGui.Text(text);
+                }
+            }
+        }
+        else {
+            const string text = "No quests available";
+            var textSize = ImGui.CalcTextSize(text);
+            ImGui.SetCursorPosX(ImGui.GetContentRegionAvail().X / 2.0f - textSize.X / 2.0f);
+            ImGui.SetCursorPosY(ImGui.GetContentRegionAvail().Y / 2.0f - textSize.Y / 2.0f);
+            ImGui.TextColored(KnownColor.Orange.Vector(), text);
+        }
+    }
+}
